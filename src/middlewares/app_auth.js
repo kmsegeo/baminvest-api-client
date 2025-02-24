6795fc9c694f88f71d4a8810496b1dd9ab71adf0
Serge KOUAME <ok@mediasoftci.com>
2025-02-24 11:01:27 +0000

    update authorisation aa

--- src/middlewares/app_auth.js
+++ src/middlewares/app_auth.js
@@ -1,12 +1,15 @@
-const { createHash } = require('crypto');
 const response = require("./response");
+const Canal = require('../models/Canal');
+const TypeOperation = require('../models/TypeOperation');
+const crypto = require('crypto');
+const Encryption = require("../utils/encryption.methods");
 
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
 
     try {
         console.log(`Authentification application..`);
         const app_hash = req.headers.appauth;
-        const req_hash = appVerifyer(req.headers.app_id, req.headers.op_code, req.headers.timestamp)
+        const req_hash = await appVerifyer(req.headers.app_id, req.headers.op_code, req.headers.timestamp)
         console.log(`Vérification du hash`)
         if (app_hash!=req_hash) 
             throw `Authentification de l'application à échoué !`;
@@ -18,16 +21,22 @@ module.exports = (req, res, next) => {
     }
 }
 
-function appVerifyer(app_id, op_code, timestamp) {
+async function appVerifyer(app_id, op_code, timestamp) {
 
-    const app_mdp = 'rdt!8vz5';
+    let canal = null;
 
     console.log(`Vérification du canal`);
-    /* à faire */
-    
-    console.log(`Vérification du code d'opération`);
-    /* à faire */
+    await Canal.findByCode(app_id).then(async result => {
+        if (!result) throw `Canal introuvable !`;
+        canal = result;
+    }).catch(err => { throw err });
 
-    const str = op_code + app_id + timestamp + app_mdp;
-    return createHash("SHA256").update(str).digest('base64');
+    console.log(`Vérification du code type opération`);
+    await TypeOperation.findByCode(op_code).then(async type_operation => {
+        if (!type_operation) throw `Erreur de code opération`;        
+    }).catch(err => { throw err });
+
+    console.log(`Décryptage du mote de passe`);
+    const app_mdp = await Encryption.decrypt(canal.r_pass);
+    return crypto.createHash("SHA256").update(op_code + app_id + timestamp + app_mdp).digest('base64');
 }
