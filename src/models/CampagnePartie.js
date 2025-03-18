10d1e39488bf36d044c0d11df24a8cd0b1cdcdff
Serge KOUAME <ok@mediasoftci.com>
2025-03-18 11:06:31 +0000

    update profil risque order response

--- src/models/CampagnePartie.js
+++ src/models/CampagnePartie.js
@@ -26,7 +26,7 @@ const CampagnePartie = {
         const queryString = `
             SELECT r_i, r_reference, r_intitule, r_ordre, r_description, r_points_totale 
             FROM ${this.tableName} 
-            WHERE e_campagne=$1 AND r_statut=$2`;
+            WHERE e_campagne=$1 AND r_statut=$2 ORDER BY r_i ASC`;
         const res = db.query(queryString, [campagne, 1]);
         return (await res).rows;
     }
