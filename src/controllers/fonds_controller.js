d29fbfd2c28d12ff740831cd10bea99d4b2f0d53
Serge KOUAME <ok@mediasoftci.com>
2025-04-02 17:25:57 +0000

    calcul des valeurs liquidatives

--- src/controllers/fonds_controller.js
+++ src/controllers/fonds_controller.js
@@ -42,6 +42,70 @@ const getAllFonds = async (req, res, next) => {
         // }
 }
 
+const getAllValeurLiquidatives = async (req, res, next) => {
+
+    const apikey = req.apikey.r_valeur;
+    const url  = `${process.env.ATSGO_URL + process.env.URI_VLS}?ApiKey=${apikey}`;
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            if (data.status!=200) return response(res, 403, `Une erreur lors de la récupération des fonds !`)
+
+            for (let payLoad of data.payLoad) {
+                
+            }
+
+            return response(res, 200, `Chargement des valeurs liquidatives`, data.payLoad)
+        })
+}
+
+const getVlFonds = async (req, res, next) => {
+
+    console.log(`Calcul des valeurs liquidatives de fonds..`)
+    
+    const apikey = req.apikey.r_valeur;
+    const fonds_url  = `${process.env.ATSGO_URL + process.env.URI_FONDS}?ApiKey=${apikey}`;
+    const vls_url  = `${process.env.ATSGO_URL + process.env.URI_VLS}?ApiKey=${apikey}`;
+
+    await fetch(fonds_url).then(async res => res.json()).then(async data => {
+        if (data.status!=200) return response(res, 403, `Une erreur lors de la récupération des fonds !`)
+        const fonds = data.payLoad;
+        await fetch(vls_url).then(res => res.json()).then(data => {
+            if (data.status!=200) return response(res, 403, `Une erreur lors de la récupération des valeurs liquidatives !`)
+            const vls = data.payLoad;
+            let result = [];
+            let i = 0;
+            for (let f of fonds) {
+                
+                let analytics = {};
+                analytics['idFcp'] = f.idFcp;
+                analytics['libelle'] = f.libelle;
+
+                let vl = {};
+                let n = 0;
+                for (let d of vls) {
+                    if (f.idFcp==d.idFcp) {
+                        vl['n-'+n] = Number(d.vl);
+                        if (n==0) analytics['valeur_liquidative'] = Number((d.vl).toFixed(2));
+                        n++;
+                    }
+                    if (n==2) break;
+                }
+                
+                analytics['progression'] = ((vl['n-0'] - vl['n-1'])/vl['n-1']).toFixed(4) + '%';
+                result[i] = analytics;
+                i++;
+            }
+
+            return response(res, 200, `Calcul des valeurs liquidatives des fonds terminé`, vls, result)
+        })
+    })
+
+}
+
 module.exports = {
-    getAllFonds
+    getAllFonds,
+    getAllValeurLiquidatives,
+    getVlFonds,
 }
--- src/routes/fonds_routes.js
+++ src/routes/fonds_routes.js
@@ -4,9 +4,8 @@ const app_auth = require('../middlewares/app_auth');
 const session_verify = require('../middlewares/session_verify')
 const fondsController = require('../controllers/fonds_controller');
 const atsgo_auth = require('../middlewares/atsgo_auth');
-const vlController = require('../controllers/valeur_liquidative_controller');
 
 router.get('/', app_auth, session_verify, atsgo_auth, fondsController.getAllFonds);
-router.get('/valeur_liquidatives', app_auth, session_verify, atsgo_auth, vlController.getAllValeurLiquidatives);
+router.get('/valeur_liquidatives', app_auth, session_verify, atsgo_auth, fondsController.getVlFonds);
 
 module.exports = router;
