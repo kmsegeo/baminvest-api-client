61ad90399910261e83f16e6311da2277919ce80b
Serge KOUAME <ok@mediasoftci.com>
2025-04-11 11:59:25 +0000

    update uploads #

--- src/controllers/onbording_controller.js
+++ src/controllers/onbording_controller.js
@@ -488,7 +488,7 @@ const uploadDomiciliation = async (req, res, next) => {
 
     const acteur = req.params.acteurId;
     const typedoc_intitule = "domiciliation";
-    const nom_fichier = `${req.protocol}://${req.get('host')}/uploads/${req.file.filename}`;
+    const nom_fichier = `${req.protocol}://${req.get('host')}/apibam/uploads/${req.file.filename}`;
 
     await TypeDocument.findByIntitule(typedoc_intitule).then(async typedoc => {
         if(!typedoc) return response(res, 404, `Le type document '${typedoc_intitule}' introuvable !`);
