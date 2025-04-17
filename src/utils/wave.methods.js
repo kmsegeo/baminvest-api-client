5d2202ec319afe08a7d2145342c01cb34c2fde9a
Serge KOUAME <ok@mediasoftci.com>
2025-04-17 11:00:12 +0000

    implement wave paiement update #2

--- src/utils/wave.methods.js
+++ src/utils/wave.methods.js
@@ -11,8 +11,8 @@ const Wave = {
             amount: montant,
             currency: "XOF",
             restrict_payer_mobile: mobile_payeur ? '+' + mobile_payeur : null,
-            error_url: url_erreur ? url_erreur : null,
-            success_url: url_succes ? url_succes : null,
+            error_url: url_erreur ? url_erreur : "https://example.com/error",
+            success_url: url_succes ? url_succes : "https://example.com/success",
         }
 
         axios.post(url, checkout_params, {
