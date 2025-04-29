5404c64302c7ebd95defb5e604c2b5125f0355f4
Serge KOUAME <ok@mediasoftci.com>
2025-04-29 17:24:19 +0000

    update webhook config #2

--- src/routes/webhook_routes.js
+++ src/routes/webhook_routes.js
@@ -4,7 +4,7 @@ const app_auth = require('../middlewares/app_auth');
 const session_verify = require('../middlewares/session_verify')
 const operationController = require('../controllers/operation_controller')
 
-router.post('/wave/checkout/payment-received', app_auth, session_verify, operationController.opSouscriptionCompleted);
+router.post('/wave/checkout/payment-received', operationController.opSouscriptionCompleted);
 
 // router.post('/rachat', app_auth, session_verify, operationController.opRachat);
 // router.post('/transfert', app_auth, session_verify, operationController.opTransfert);
