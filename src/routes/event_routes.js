9aa7f4367f60224320cc3e234a1f1931ea475220
Serge KOUAME <serge.kouame@groupemediasoft.com>
2025-07-23 13:00:22 +0000

    open sockets to operations & transactions #9

--- src/routes/event_routes.js
+++ src/routes/event_routes.js
@@ -61,17 +61,19 @@ router.get('/acteurs/operations', app_auth, session_verify, atsgo_auth, async (r
 
             // Chargement pour la première fois
 
-            // await Operation.findAllByActeur(acteur_id).then(async operations => {
-            //     await fetch(url)
-            //     .then(async res => res.json())
-            //     .then(async data => {
-            //         if (data.status!=200) return response(res, 403, `Une erreur lors de la récupération des opération !`);
-            //         for(let payLoad of data.payLoad) delete payLoad.idClient;
-            //         res.write(`data: ${JSON.stringify({statut: "SUCCESS", message: `Dernière récupération des opérations: ${new Date().toLocaleString()}`, data: data.payLoad})}\n\n`);
-            //         res.flushHeaders();
-            //         cur_operations = operations;
-            //     }).catch(err => next(err));
-            // }).catch(err=>next(err));
+            await Operation.findAllByActeur(acteur_id).then(async operations => {
+                await fetch(url)
+                .then(async res => res.json())
+                .then(async data => {
+                    if (data.status!=200) return response(res, 403, `Une erreur lors de la récupération des opération !`);
+                    for(let payLoad of data.payLoad) delete payLoad.idClient;
+                    setTimeout(()=>{
+                        res.write(`data: ${JSON.stringify({statut: "SUCCESS", message: `Dernière récupération des opérations: ${new Date().toLocaleString()}`, data: data.payLoad})}\n\n`);
+                        res.flushHeaders();
+                    },2000)
+                    cur_operations = operations;
+                }).catch(err => next(err));
+            }).catch(err=>next(err));
             
             // Chargement à un interval défini
             
