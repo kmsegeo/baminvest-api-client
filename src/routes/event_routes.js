85d8c44a084361ec0750715c19fc8f2ee86ce333
Serge KOUAME <serge.kouame@groupemediasoft.com>
2025-07-23 15:49:38 +0000

    open sockets to operations & transactions #20

--- src/routes/event_routes.js
+++ src/routes/event_routes.js
@@ -8,33 +8,6 @@ const Operation = require('../models/Operation');
 const Acteur = require('../models/Acteur');
 const { Particulier } = require('../models/Client');
 
-router.get('/', (req, res, next) => {
-
-    console.log(`Ouverture du socket..`)
-
-    // Set headers to keep the connection alive and tell the client we're sending event-stream data
-    res.setHeader('Content-Type', 'text/event-stream');
-    res.setHeader('Cache-Control', 'no-cache');
-    res.setHeader('Connection', 'keep-alive');
-
-    // Send an initial message
-    res.write(`data: Connected to server\n\n`);
-
-    // Simulate sending updates from the server
-    let counter = 0;
-    const intervalId = setInterval(() => {
-        counter++;
-        // Write the event stream format
-        res.write(`data: Message ${counter}\n\n`);
-    }, 2000);
-
-    // When client closes connection, stop sending events
-    req.on('close', () => {
-        clearInterval(intervalId);
-        res.end();
-    });
-});
-
 router.get('/acteurs/operations', app_auth, session_verify, atsgo_auth, async (req, res, next) => {
 
     console.log('Ouverture de socket: Historique des opérations..')
@@ -42,6 +15,11 @@ router.get('/acteurs/operations', app_auth, session_verify, atsgo_auth, async (r
     res.setHeader('Content-Type', 'text/event-stream');
     res.setHeader('Cache-Control', 'no-cache');
     res.setHeader('Connection', 'keep-alive');
+
+    const sendEvent = (data) => {
+        res.write(`data: ${JSON.stringify({statut: "SUCCESS", message: `Dernière récupération des opérations: ${new Date().toLocaleString()}`, data})}\n\n`);
+        res.flushHeaders();
+    };
 
     const acteur_id = req.session.e_acteur;
     
