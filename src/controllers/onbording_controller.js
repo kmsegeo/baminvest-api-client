85ff720a05924176c322df67b4057775d9db8ee0
Serge KOUAME <serge.kouame@groupemediasoft.com>
2025-08-08 19:40:49 +0000

    onbording patch iclient introuvable #4

--- src/controllers/onbording_controller.js
+++ src/controllers/onbording_controller.js
@@ -748,30 +748,20 @@ const verifierOtp = async (req, res, next) => {
                                     // "idClientParent": 0,
                                     // "idSecteurActivite": 3
                                 }, async (atsgo_data) => {
-
-                                    console.log(`Données avant :`);
-                                    console.log(particulier)
                                     
                                     await Atsgo.validateAtsgoAccount(apikey, atsgo_data).then(() => {
 
-                                        Acteur.findByEmail(atsgo_data.email).then(async acteur => {
-                                            if (!acteur) throw `Acteur non trouvé !`;
-                                            await Client.Particulier.setAtsgoCallbackData(acteur.e_particulier, atsgo_data.idClient, atsgo_data.code, atsgo_data.numeroCompteTitre).then(async updated_particulier => {
-                                                if (!particulier) throw `Erreur à l'enregistrement du compte titre !`;
-                                                console.log('Données du callback envoyé');
-                                                console.log(updated_particulier);
-                                                // Acteur.activeCompte(acteur_id).catch(err => next(err));
-                                            }).catch(error => { throw error });
+                                        Client.Particulier.setAtsgoCallbackData(acteur.e_particulier, atsgo_data.idClient, atsgo_data.code, atsgo_data.numeroCompteTitre).then(updated_particulier => {
+                                            if (!updated_particulier) throw `Erreur à l'enregistrement du compte titre !`;
+                                            console.log('Données du callback envoyé');
+                                            Acteur.activeCompte(acteur_id).catch(err => next(err));
+                                            OTP.confirm(acteur_id, otp.r_i).catch(err => next(err)); 
+                                            return response(res, 200, `Vérification terminé avec succès`);
                                         }).catch(error => { throw error });
 
                                     }).catch(err => next(err));
                                     
-                                    await OTP.confirm(acteur_id, otp.r_i).catch(err => next(err)); 
-
-                                    return response(res, 200, `Vérification terminé avec succès`);
-
                                 }).catch(async err => response(res, 403, err));
-
                             }).catch(err => next(err));
                         }).catch(err => next(err));
 
